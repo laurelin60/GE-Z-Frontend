a58ef13a9fe07d1d6343529c3b1884c88dc5509b
Kevin Wu <kevinwu098@gmail.com>
2023-11-14 19:02:35 -0800

    Add responsiveness to header

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { FaUser } from "react-icons/fa";
 
 const Header = () => {
     return (
         <>
-            <div className="flex h-24 items-center px-24">
-                <div className="text-primary text-4xl font-bold">Degree-EZ</div>
+            <div className="flex h-24 place-content-center items-center justify-between px-8 md:px-24">
+                <div className="text-4xl font-bold text-primary">Degree-EZ</div>
+
+                <div className="flex h-12 w-12 place-content-center items-center rounded-full border-2 border-primary border-opacity-40 text-2xl shadow-xl md:h-16 md:w-16 md:text-3xl">
+                    <FaUser />
+                </div>
             </div>
         </>
     );
