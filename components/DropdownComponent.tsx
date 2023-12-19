b33e945bbacb6aae377847e5faca071c352d5bc6
Kevin Wu <kevinwu098@gmail.com>
2023-12-18 19:48:39 -0800

    style: adjust border roundness

--- components/DropdownComponent.tsx
+++ components/DropdownComponent.tsx
@@ -23,7 +23,7 @@ export const DropdownComponentHero = (props: DropdownComponentProps) => {
                 <select
                     value={value}
                     onChange={handleChange}
-                    className="block h-full w-full appearance-none overflow-ellipsis rounded-2xl border-4 border-black px-4 pr-12 text-lg opacity-40 focus:border-primary focus:opacity-80 md:pr-16 md:text-2xl"
+                    className="block h-full w-full appearance-none overflow-ellipsis rounded-xl border-4 border-black px-4 pr-12 text-lg opacity-40 focus:border-primary focus:opacity-80 md:pr-16 md:text-2xl"
                 >
                     {data.map((item) => (
                         <option key={item}>{item}</option>
@@ -53,7 +53,7 @@ export const DropdownComponentSearch = (props: DropdownComponentProps) => {
                 <select
                     value={value}
                     onChange={handleChange}
-                    className="block h-full w-full appearance-none overflow-ellipsis rounded-2xl border-4 border-black px-4 pr-12 text-lg opacity-40 focus:border-primary focus:opacity-80 md:pr-16 md:text-2xl"
+                    className="block h-full w-full appearance-none overflow-ellipsis rounded-xl border-4 border-black px-4 pr-12 text-lg opacity-40 focus:border-primary focus:opacity-80 md:pr-16 md:text-2xl"
                 >
                     {data.map((item) => (
                         <option key={item}>{item}</option>
