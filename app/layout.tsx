14f45e62b2851aa710b8107fa94bad43868c0cd8
Kevin Wu <kevinwu098@gmail.com>
2023-12-27 18:34:20 -0800

    feat: add google analytics (#30)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
             <body className={inter.className}>
                 <div className="flex flex-col place-content-center bg-background text-text">
                     <Header />
+                    <GoogleAnalytics />
                     {children}
                     <Footer />
                 </div>
---
+++ lib/gtag.ts
@@ -0,0 +1,27 @@
+// https://stackoverflow.com/questions/77063977/implementing-google-analytics-in-nextjs-13-app-directory
+
+export const GA_TRACKING_ID: string | undefined = process.env.GA_TRACKING_ID;
+
+export const pageview = (url: string) => {
+    (window as any).gtag("config", GA_TRACKING_ID, {
+        page_path: url,
+    });
+};
+
+export const event = ({
+    action,
+    category,
+    label,
+    value,
+}: {
+    action: string;
+    category: string;
+    label: string;
+    value: number;
+}) => {
+    (window as any).gtag("event", action, {
+        event_category: category,
+        event_label: label,
+        value: value,
+    });
+};
