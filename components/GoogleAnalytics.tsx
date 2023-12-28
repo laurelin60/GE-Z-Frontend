84c7a88da4d0b5bb8bf070229ad4132493fef3f0
Kevin Wu <kevinwu098@gmail.com>
2023-12-27 18:55:07 -0800

    fix: update env name

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -8,7 +8,7 @@ const GoogleAnalytics = () => {
         <>
             <Script
                 strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.NEXT_PUBLIC_GA_TRACKING_ID}`}
             />
             <Script
                 id="gtag-init"
@@ -18,7 +18,7 @@ const GoogleAnalytics = () => {
                       window.dataLayer = window.dataLayer || [];
                       function gtag(){dataLayer.push(arguments);}
                       gtag('js', new Date());
-                      gtag('config', '${gtag.GA_TRACKING_ID}', {
+                      gtag('config', '${gtag.NEXT_PUBLIC_GA_TRACKING_ID}', {
                       page_path: window.location.pathname,
                       });
                     `,
--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,9 +1,10 @@
 // https://stackoverflow.com/questions/77063977/implementing-google-analytics-in-nextjs-13-app-directory
 
-export const GA_TRACKING_ID: string | undefined = process.env.GA_TRACKING_ID;
+export const NEXT_PUBLIC_GA_TRACKING_ID: string | undefined =
+    process.env.NEXT_PUBLIC_GA_TRACKING_ID;
 
 export const pageview = (url: string) => {
-    (window as any).gtag("config", GA_TRACKING_ID, {
+    (window as any).gtag("config", NEXT_PUBLIC_GA_TRACKING_ID, {
         page_path: url,
     });
 };
