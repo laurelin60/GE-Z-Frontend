5a1b728f216226ea0d5d327152e613f3d4981078
Kevin Wu <kevinwu098@gmail.com>
2024-04-03 11:07:03 -0700

    Revert "chore: replace gtag implementation w/ next.js" (#51)
    Revert "chore: replace gtag implementation w/ next.js (#49)"
    
    This reverts commit 9a4037d047ed2fb1e3485e12be955ee85fa8f0a3.

---
+++ components/GoogleAnalytics.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Script from "next/script";
+import * as gtag from "@/lib/gtag";
+
+const GoogleAnalytics = () => {
+    return (
+        <>
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.NEXT_PUBLIC_GA_TRACKING_ID}`}
+            />
+            <Script
+                id="gtag-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', '${gtag.NEXT_PUBLIC_GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                      });
+                    `,
+                }}
+            />
+        </>
+    );
+};
+
+export default GoogleAnalytics;
