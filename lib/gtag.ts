703ea60fa1b9f4c84c469d8d9a266099c1bc9587
Kevin Wu <kevinwu098@gmail.com>
2024-04-03 11:09:06 -0700

    chore: fix eslint errors

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -4,6 +4,7 @@ export const NEXT_PUBLIC_GA_TRACKING_ID: string | undefined =
     process.env.NEXT_PUBLIC_GA_TRACKING_ID;
 
 export const pageview = (url: string) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (window as any).gtag("config", NEXT_PUBLIC_GA_TRACKING_ID, {
         page_path: url,
     });
