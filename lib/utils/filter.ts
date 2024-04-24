e4a07b52c42de6f0e2ce2a0a0aa54d52ea65e982
Kevin Wu <kevinwu098@gmail.com>
2024-04-23 23:03:24 -0700

    fix: correct filter

--- lib/utils/filter.ts
+++ lib/utils/filter.ts
@@ -39,30 +39,18 @@ export function filterData(
             (filterValues.format[0] && filterValues.format[1]) ||
             (result.async && filterValues.format[0]) ||
             (!result.async && filterValues.format[1]);
-
-        const instantEnrollment = filterValues.enrollment[0]
-            ? result.instantEnrollment
-            : true;
         const hasOpenSeats = filterValues.available[0]
             ? result.hasOpenSeats
             : true;
         const teachingInstitution =
             result.sendingInstitution == filterValues.institution ||
             filterValues.institution == "Any Institution";
-        const withinUnits =
-            result.units >= filterValues.min &&
-            result.units <= filterValues.max;
         const withinTime =
             startsAfter(filterValues.start, result) &&
             endsBefore(filterValues.end, result);
 
         return (
-            onlineFormat &&
-            instantEnrollment &&
-            hasOpenSeats &&
-            teachingInstitution &&
-            withinUnits &&
-            withinTime
+            onlineFormat && hasOpenSeats && teachingInstitution && withinTime
         );
     });
 
