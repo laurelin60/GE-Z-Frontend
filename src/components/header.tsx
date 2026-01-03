8e403c55e990183dc1229289b6d02b2eca1e6708
Kevin Wu <kevinwu098@gmail.com>
2026-01-03 15:47:04 -0800

    feat: logo header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
+import { GraduationCapIcon } from "lucide-react";
 
 export function Header() {
     return (
-        <header className="bg-background box-border h-fit px-4 py-4 leading-none shadow-sm md:px-16">
-            <Link href="/">
-                <div className="text-mono text-primary text-4xl font-bold">
-                    GE-Z
+        <header className="bg-background box-border h-fit px-4 py-4 leading-none shadow-xs md:px-16">
+            <Link
+                href="/"
+                className="flex w-fit"
+            >
+                <div className="outline-border/80 w-fit rounded-md p-1.5 outline-2">
+                    <GraduationCapIcon className="text-primary size-8" />
                 </div>
             </Link>
         </header>
